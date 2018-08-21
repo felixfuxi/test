7de94d2287808bdec01b6d53bd9494de0dbfa2eb
felixfu <curiosity.fu@gmail.com>
2018-08-21 11:02:12 +0800

    修改了一些缺陷

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class HelloWorld {
+  
+  
+  public static void main(String[] args) { 
+    
+    System.out.println("Hello World! 中文测试");
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
